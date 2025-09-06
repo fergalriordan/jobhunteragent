76463e82c9ff0516d79394cea1b532744a1e9892
Fergal Riordan <fergalriordan333@gmail.com>
2025-09-06 14:38:53 +0100

    Re-introduce pdf generation

--- tools/convert_to_pdf.py
+++ tools/convert_to_pdf.py
@@ -2,10 +2,10 @@ import subprocess
 from langchain_core.tools import tool
 
 @tool
-def convert_to_pdf(path_to_word_doc: str) -> bool: 
+def convert_to_pdf(path_to_word_doc: str, path_to_output_pdf: str) -> bool: 
     """Convert Word doc to PDF using docx2pdf CLI"""
     try:
-        subprocess.run(["docx2pdf", path_to_word_doc, "tailored_cv.pdf"], check=True)
+        subprocess.run(["docx2pdf", path_to_word_doc, path_to_output_pdf], check=True)
         return True
     except Exception:
         return False
